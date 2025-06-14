52c41a1569bfbcf0b7fe918dbbbab29e5c8f9121
Blackburn0 <hamzatadebayo5@gmail.com>
2025-06-14 09:54:24 +0100

    deployment

--- config/settings.py
+++ config/settings.py
@@ -59,6 +59,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # added
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -106,6 +108,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 
 tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
+print("Parsed path:", tmpPostgres.path)
 
 DATABASES = {
     'default': {
@@ -160,7 +163,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'assets'),
