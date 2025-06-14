6c4664f8c696f5a0fdb25acd7378ace466bab6e9
Blackburn0 <hamzatadebayo5@gmail.com>
2025-06-14 09:15:41 +0100

    deployment

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",") or []
+# CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",") or []
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
