49a230d4ff4d4cafbf5f25b9df35a90a0068b3ee
Blackburn0 <hamzatadebayo5@gmail.com>
2025-06-18 10:09:40 +0100

    fix: CSRF error

--- config/settings.py
+++ config/settings.py
@@ -33,8 +33,12 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = ["*"]
 
+# ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS")
+# CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS")
+
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS", "").split(",")
 
 # Application definition
 
